0148d61b1ed1acec72cb8250f8a4a1a46562d8c7
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-06-24 23:26:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,7 +120,7 @@ client.on('message', async function(message) {
         const permissions = voiceChannel.permissionsFor(message.client.user)
         if (!permissions.has('CONNECT')) return message.channel.send({embed: {description: "🛑 I don't have permission to CONNECT! Give me some."}});
         if (!permissions.has('SPEAK')) return message.channel.send({embed: {description: "🛑 I don't have permission to SPEAK! Give me some."}});
-         if (args.length == 0 || !args) return message.channel.send(`:musical_note: ❯ m-play **Youtube URL / Search**`)
+         if (args.length == 0 || !args) return message.channel.send(`:musical_note: ❯ 1play **Youtube URL / Search**`)
             if (guilds[message.guild.id].queue.length > 0 || guilds[message.guild.id].isPlaying) {
                 if(guilds[message.guild.id].queue.length > 100) return message.channel.send(``, {embed: {
                     description: `🔒 Sorry, max queue length is 100, do **${prefix}clear** to clear entire queue or **${prefix}clear <number>** to clear 1 item`
